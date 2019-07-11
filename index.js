88d20e2f56b05f803fb1105c38aa1ffbc7c79905
林贵华 <710681042@qq.com>
2019-07-11 16:47:23 +0800

    1.修复docker因路径问题导致静态资源服务器访问不了问题。

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.all('*', function(req, res, next) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use("/", indexRouter);
 
 
